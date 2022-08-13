2bd95c53947e70c01b99571a5ab187c459e100c8
Ishan Sawant <2019meb1265@iitrpr.ac.in>
2022-08-13 15:40:48 +0530

    Scene addition
    Added - main scene, customization scene and levels scene
    Added ScoreSystem.cs for levels

---
+++ BasketBall2D/Assets/Scripts/Main/MainButtonsManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainButtonsManager : MonoBehaviour
+{
+    public void Play() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void HowToPlay() {
+        //show instructions
+    }
+
+    public void Customize() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Options() {
+        //Show Options
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+
+}
